ad9aaae2afbdaf1bc31dc00878094694cddb1e41
Yana Ushakova <junk7day@gmail.com>
2022-11-30 14:47:20 +0300

    Delete LinearLayout

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -13,39 +12,14 @@ import com.example.shoppinglist.domain.ShopItem
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var linearLayoutShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        linearLayoutShopList = findViewById(R.id.LinearLayout_shopList)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
-            showList(it)
+
         }
     }
 
-    private fun showList(list: List<ShopItem>) {
-
-        linearLayoutShopList.removeAllViews()
-
-        for (shopItem in list) {
-            val layoutId = if (shopItem.enabled) R.layout.item_shop_enabled
-            else R.layout.item_shop_disabled
-            val view = LayoutInflater.from(this)
-                .inflate(layoutId, linearLayoutShopList, false)
-            val textViewName = view.findViewById<TextView>(R.id.textView_name)
-            val textViewWeight = view.findViewById<TextView>(R.id.textView_weight)
-            val textViewCount = view.findViewById<TextView>(R.id.textView_count)
-            textViewName.text = shopItem.name
-            textViewWeight.text = shopItem.weight.toString()
-            textViewCount.text = shopItem.count.toString()
-
-            view.setOnLongClickListener {
-                viewModel.changeEnableState(shopItem)
-                true
-            }
-            linearLayoutShopList.addView(view)
-        }
-    }
 }
