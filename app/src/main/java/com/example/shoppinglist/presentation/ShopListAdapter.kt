b161158dc402a7bed9a53756134dbf25884573a5
Yana Ushakova <junk7day@gmail.com>
2022-11-30 12:35:42 +0300

    Edit ShopItemViewHolder

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -2,7 +2,9 @@ package com.example.shoppinglist.presentation
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
@@ -21,5 +23,9 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
         return list.size
     }
 
-    class ShopItemViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val textViewName = view.findViewById<TextView>(R.id.textView_name)
+        val textViewWeight = view.findViewById<TextView>(R.id.textView_weight)
+        val textViewCount = view.findViewById<TextView>(R.id.textView_count)
+    }
 }
