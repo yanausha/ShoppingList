9775e21c315b23e4a107b11a867ebc2258e14f1a
Yana Ushakova <junk7day@gmail.com>
2022-11-30 12:21:59 +0300

    Create listOf<ShopItem>

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -3,8 +3,11 @@ package com.example.shoppinglist.presentation
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
         TODO("Not yet implemented")
