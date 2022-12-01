db4336774ba64eac1c208a465d27e47d8379ea77
Yana Ushakova <junk7day@gmail.com>
2022-12-01 15:20:22 +0300

    override areItemsTheSame()

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemDiffCallback.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemDiffCallback.kt
@@ -6,7 +6,7 @@ import com.example.shoppinglist.domain.ShopItem
 class ShopItemDiffCallback: DiffUtil.ItemCallback<ShopItem>() {
 
     override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
-        TODO("Not yet implemented")
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
