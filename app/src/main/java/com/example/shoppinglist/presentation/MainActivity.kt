a18f802089539ddd4cb2fc94ddf5af1a68dd08d6
Yana Ushakova <junk7day@gmail.com>
2022-12-01 11:19:37 +0300

    Edit adapter

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.shoppinglist.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var shopListAdapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +17,24 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerView()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
-            adapter.shopList = it
+            shopListAdapter.shopList = it
         }
     }
 
     private fun setupRecyclerView() {
         val recyclerViewShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
-        adapter = ShopListAdapter()
-        recyclerViewShopList.adapter = adapter
-        recyclerViewShopList.recycledViewPool.setMaxRecycledViews(
-            ShopListAdapter.ENABLED_ITEM,
-            ShopListAdapter.MAX_POOL_SIZE
-        )
-        recyclerViewShopList.recycledViewPool.setMaxRecycledViews(
-            ShopListAdapter.DISABLED_ITEM,
-            ShopListAdapter.MAX_POOL_SIZE
-        )
+        with(recyclerViewShopList) {
+            shopListAdapter = ShopListAdapter()
+            adapter = shopListAdapter
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.ENABLED_ITEM,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.DISABLED_ITEM,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
     }
 
 }
