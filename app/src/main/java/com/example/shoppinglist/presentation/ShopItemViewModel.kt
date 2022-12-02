b3f00633bbd3ea552564380b723a433691ab3936
Yana Ushakova <junk7day@gmail.com>
2022-12-02 15:46:59 +0300

    Add getShopItem()

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -5,6 +5,7 @@ import com.example.shoppinglist.data.ShopListRepositoryImpl
 import com.example.shoppinglist.domain.AddShopItemUseCase
 import com.example.shoppinglist.domain.EditShopItemUseCase
 import com.example.shoppinglist.domain.GetShopItemUseCase
+import com.example.shoppinglist.domain.ShopItem
 
 class ShopItemViewModel: ViewModel() {
 
@@ -13,5 +14,9 @@ class ShopItemViewModel: ViewModel() {
     private val getShopItemUseCase = GetShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getItemId(shopItemId)
+    }
 
 }
