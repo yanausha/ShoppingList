afa48aa940f0e217fe6ef907e66591d43c447b65
Yana Ushakova <junk7day@gmail.com>
2022-12-07 14:50:40 +0300

    Edit addTextChangeListeners()

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -3,6 +3,8 @@ package com.example.shoppinglist.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
