064f7781117ad9259593950122bf3fcf0d448dc4
Yana Ushakova <junk7day@gmail.com>
2022-12-08 15:08:58 +0300

    Add check null

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -16,7 +16,7 @@ class ShopItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
         parseIntent()
-        launchRightMode()
+        if (savedInstanceState == null) launchRightMode()
     }
 
     private fun launchRightMode() {
